8e0f5529286e84287522fdf52f0d0562d702e9c0
JeremyVanGrack <Jeremyv1@umbc.edu>
2022-04-02 14:16:12 -0400

    making sure I can commit to the github

---
+++ SafeHaven/Haven/models.py
@@ -0,0 +1,15 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class EvacLocation(models.Model):
+    address = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.address
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
