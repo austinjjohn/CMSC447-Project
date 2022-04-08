d0ce25d12a3bb874d600fffecef7e5acd33a75b3
JeremyVanGrack <Jeremyv1@umbc.edu>
2022-04-08 11:35:50 -0400

    Login view now has button, input fields wont show up. Trying to make index view render() work.

--- SafeHaven/Haven/views.py
+++ SafeHaven/Haven/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
+    #return render(request, 'base.html')
     return HttpResponse("Hello, world. You're at the Haven index.")
 
 def login(request):
-    return HttpResponse("LOGIN.")
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render({}, request))
