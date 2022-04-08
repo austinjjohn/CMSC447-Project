78acb26622032137321b42507a007d6c99ac7993
JeremyVanGrack <Jeremyv1@umbc.edu>
2022-04-08 16:56:14 -0400

    created working navigation bar. Fixed issue with false Haven/ homepage.

--- SafeHaven/Haven/views.py
+++ SafeHaven/Haven/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.contrib.auth import authenticate, login
+
+#from .models import Evacuee, Volunteer, EvacLocation
 
 # Create your views here.
+
 
 def index(request):
     template = loader.get_template("base.html")
     return HttpResponse(template.render({}, request))
     #return HttpResponse("Hello, world. You're at the Haven index.")
+
 
 def login(request):
     template = loader.get_template("login.html")
