bc447c72e9db28ea2f60943bbb38192edc89e8d6
JeremyVanGrack <Jeremyv1@umbc.edu>
2022-04-08 13:11:38 -0400

    Header of Haven index is now showing up. Login buttons made and working

--- SafeHaven/Haven/views.py
+++ SafeHaven/Haven/views.py
@@ -5,8 +5,9 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    #return render(request, 'base.html')
-    return HttpResponse("Hello, world. You're at the Haven index.")
+    template = loader.get_template("base.html")
+    return HttpResponse(template.render({}, request))
+    #return HttpResponse("Hello, world. You're at the Haven index.")
 
 def login(request):
     template = loader.get_template("login.html")
