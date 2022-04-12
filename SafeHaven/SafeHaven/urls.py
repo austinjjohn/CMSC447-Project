56492290df6bfa53764687bfb05ab3f2ccf65920
Derek Marx <dmarx1@umbc.edu>
2022-04-12 19:30:29 -0400

    Initial saver_users push

--- SafeHaven/SafeHaven/urls.py
+++ SafeHaven/SafeHaven/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('Haven.urls')),
-    # path('Haven/', include('Haven.urls')),
+    path('Haven/', include('Haven.urls')),
     path('admin/', admin.site.urls),
 ]
