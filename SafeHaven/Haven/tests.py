d1cd90dbeab6f0054ddf97382a50f1f222ac6d79
JeremyVanGrack <Jeremyv1@umbc.edu>
2022-04-24 13:04:02 -0400

    finalized tests by adding volunteer and evacuee tests

--- SafeHaven/Haven/tests.py
+++ SafeHaven/Haven/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from Haven.models import EvacLocation
 from django.utils import timezone
-
+from Haven.models import EvacLocation, Volunteer, Evacuee
 # Create your tests here.
+
 class LocationTest(TestCase):
     def setUp(self):
         pass
@@ -18,3 +18,25 @@ class LocationTest(TestCase):
         test.get_spaces()
         test.was_published_recently()
         test.is_full()
+
+class VolunteerTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_Volunteer(self):
+        volunteer = Volunteer(name="Jeremy")
+        volunteer.dict()
+
+class EvacueeTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_Evacuee(self):
+        evacuee = Evacuee(name="Jake", pets=2, spaces_needed=1)
+        evacuee.dict()
