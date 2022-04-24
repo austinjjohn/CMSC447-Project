aef1e2746b62bb7a1c391d8424d8dc6a69043f8c
JeremyVanGrack <Jeremyv1@umbc.edu>
2022-04-24 14:26:45 -0400

    changed listing view to take POST method. Added tests for all models. Added create method for EvacLocation. Changed listing creation in volunteer.html. Removed create listing button in listings.html.

--- SafeHaven/Haven/views.py
+++ SafeHaven/Haven/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template import loader
 from .forms import RegisterationForm
+from .models import EvacLocation
 
 #from .models import Evacuee, Volunteer, EvacLocation
 
@@ -24,8 +25,24 @@ def login(response):
     return render(response, "login.html", {"form": form})
 
 def listings(request):
-    template = loader.get_template("listings.html")
-    return HttpResponse(template.render({}, request))
+    # template = loader.get_template("listings.html")
+    # return HttpResponse(template.render({}, request))
+    if request.method == 'POST':
+        if request.POST.get('address') and request.POST.get('spaces'):
+            address = request.POST["address"]
+            pets = request.POST["pets"]
+            spaces = request.POST["spaces"]
+            dict = {
+                'address': address,
+                'pets': pets,
+                'spaces': spaces
+            }
+            # location = EvacLocation.create(address, pets, spaces)
+            # location.save()
+            return render(request, 'listings.html', dict)
+
+    else:
+        return render(request, 'listings.html', dict)
 
 def volunteer(request):
     template = loader.get_template("volunteer_profile_page.html")
@@ -34,3 +51,19 @@ def volunteer(request):
 def evacuee(request):
     template = loader.get_template("evac_profile_page.html")
     return HttpResponse(template.render({}, request))
+
+# def createListing(request):
+#     if request.method == 'POST':
+#         if request.POST.get('address') and request.POST.get('spaces'):
+#             address = request.POST["address"]
+#             pets = request.POST["pets"]
+#             spaces = request.POST["spaces"]
+#             dict = {
+#                 'address': address,
+#                 'pets': pets,
+#                 'spaces': spaces
+#             }
+#             return render(request, 'templates/listings.html', dict)
+#
+#     else:
+#         return render(request, 'templates/listings.html', dict)
