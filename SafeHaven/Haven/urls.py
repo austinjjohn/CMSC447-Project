acdb2ef0c2d83fb1b83bfd91d60519e38dd9bc10
austinjjohn <ajohn8@umbc.edu>
2022-05-09 01:04:47 -0400

    Switched Signup and Login. Implementing Login Auth - Login Page not working

--- SafeHaven/Haven/urls.py
+++ SafeHaven/Haven/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('login', views.login_user, name='login'),
     path('signup', views.signup, name='signup'),
     path('listings', views.listings, name='listings'),
     path('volunteer', views.volunteer, name='volunteer'),
--- SafeHaven/Haven/views.py
+++ SafeHaven/Haven/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template import loader
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterationForm
 from Haven.models import EvacLocation
-from Haven.models import Signup
+from Haven.models import Login
+
+
 # Create your views here.
 
 
@@ -12,35 +16,42 @@ def index(request):
     return HttpResponse(template.render({}, request))
     # return HttpResponse("Hello, world. You're at the Haven index.")
 
-def login(response):
+
+def signup(response):
     if response.method == "POST":
         form = RegisterationForm(response.POST)
         if form.is_valid():
             form.save()
 
-        return redirect("/listings") #UNCOMMENT WHEN WE HAVE A LISTINGS PAGE TO CONTINUE TO
+        return redirect("/listings")  # UNCOMMENT WHEN WE HAVE A LISTINGS PAGE TO CONTINUE TO
     else:
         form = RegisterationForm()
-    return render(response, "login.html", {"form": form})
+    return render(response, "signup.html", {"form": form})
 
 
-def signup(request):
+def login_user(request):
     if request.method == "POST":
-        if request.POST.get('firstname') and request.POST.get('lastname') and request.POST.get('username') and request.POST.get('password') and request.POST.get('email'):
-            firstname = request.POST["firstname"]
-            lastname = request.POST["lastname"]
-            username = request.POST["username"]
-            password = request.POST["password"]
+        if request.POST.get('email') and request.POST.get('password'):
             email = request.POST["email"]
+            password = request.POST["password"]
+            user = authenticate(request, email=email, password=password)
 
-            signup = Signup.create(firstname, lastname, username, email, password)
-            signup.save()
+            if user is not None:
+                login(request, user)
+                return redirect('homepage.html')
+            else:
+                messages.success(request, "Incorrect Email or Password. Try Again.")
+                return redirect('login.html')
 
-    data = Signup.objects.all()
-    info = {
-        "list_data": data
-    }
-    return render(request, 'signup.html', info)
+    #         logininfo = Login.create(firstname, lastname, username, email, password)
+    #         logininfo.save()
+    #
+    # data = Login.objects.all()
+    # info = {
+    #     "list_data": data
+    # }
+    else:
+        return render(request, 'login.html')
 
     # if response.method == "POST":
     #     form = RegisterationForm(response.POST)
@@ -84,6 +95,7 @@ def evacuee(request):
     template = loader.get_template("evac_profile_page.html")
     return HttpResponse(template.render({}, request))
 
+
 def createListing(request):
     if request.method == 'POST':
         if request.POST.get('address') and request.POST.get('spaces'):
@@ -97,7 +109,7 @@ def createListing(request):
             }
     return render(request, 'templates/listings.html', dict)
 
+
 def map(request):
     template = loader.get_template("map.html")
     return HttpResponse(template.render({}, request))
-
