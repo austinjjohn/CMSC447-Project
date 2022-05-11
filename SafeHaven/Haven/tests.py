131c8ed7a17a0380992467064730e2f7cb211c23
jmi1umbc <102565348+jmi1umbc@users.noreply.github.com>
2022-05-10 22:25:16 -0400

    Update tests.py
    added a sample test for map attributes

--- SafeHaven/Haven/tests.py
+++ SafeHaven/Haven/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
-
-# Create your tests here.
+# from django.test import TestCase
+#
+# # Create your tests here.
+# from django.urls import reverse
+#
+#
+# class MapTests(TestCase):
+#     def test_map_attributes(self):
+#         default_map_attributes = {
+#             "style": "satellite-streets-v11",
+#             "zoom": 14,
+#             "center": [-76.71095954472092, 39.25386386415346],
+#         }
+#         result = {
+#             "style": "satellite-streets-v11",
+#             "zoom": 14,
+#             "center": [-76.71095954472092, 39.25386386415346],
+#         }
+#         for key in default_map_attributes.keys():
+#             self.assertEqual(result[key], default_map_attributes[key])
