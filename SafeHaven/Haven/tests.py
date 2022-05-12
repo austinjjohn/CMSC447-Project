e6b2925df05b09b66e00b75e864320e43e359f95
jmi1umbc <102565348+jmi1umbc@users.noreply.github.com>
2022-05-11 22:35:53 -0400

    Update tests.py

--- SafeHaven/Haven/tests.py
+++ SafeHaven/Haven/tests.py
@@ -123,3 +123,18 @@ class EvacueeTest(StaticLiveServerTestCase):
 #         test.is_full()
 #
 #
+
+# class MapTests(TestCase):
+#     def test_map_attributes(self):
+#         default_map_attributes = {
+#             "style": "satellite-streets-v11",
+#             "zoom": 14,
+#             "center": [-76.71095954472092, 39.25386386415346],
+#         }
+#         result = {
+#             "style": "satellite-streets-v11",
+#             "zoom": 14,
+#             "center": [-76.71095954472092, 39.25386386415346],
+#         }
+#         for key in default_map_attributes.keys():
+#             self.assertEqual(result[key], default_map_attributes[key])
